d90fa89b610ba3e2912ba389fd1a407219b21a43
InKyoJeong <viapolar@naver.com>
2021-05-12 23:37:35 +0900

    Forkify - Implement Search

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -1,5 +1,6 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 
 import 'core-js/stable'; // is for polyfilling everything else
 import 'regenerator-runtime/runtime'; // is for polyfilling async/await
@@ -8,7 +9,6 @@ const controllRecipe = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
-
     if (!id) return;
 
     recipeView.renderSpinner();
@@ -23,8 +23,25 @@ const controllRecipe = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controllRecipe);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
