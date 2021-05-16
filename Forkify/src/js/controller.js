2a003018ce012b5f043d2313513b4cb6f704cca6
InKyoJeong <viapolar@naver.com>
2021-05-16 17:30:16 +0900

    Forkify - add to bookmark list

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable'; // is for polyfilling everything else
 import 'regenerator-runtime/runtime'; // is for polyfilling async/await
@@ -22,6 +23,7 @@ const controllRecipe = async function () {
 
     // 0) Update results view to mark selected
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading recipe
     await model.loadRecipe(id);
@@ -73,12 +75,18 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/Remove bookmark
   if (!model.state.recipe.bookmarked) {
     model.addBookmark(model.state.recipe);
   } else {
     model.deleteBookmark(model.state.recipe.id);
   }
+
+  // 2) Update recipe View
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
