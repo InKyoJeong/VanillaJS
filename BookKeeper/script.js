9f9751203ff56bad9c0eff02fcdc6bfa42b63ac2
InKyoJeong <viapolar@naver.com>
2021-06-19 23:14:29 +0900

    feat(bookmark-app): Add modal

---
+++ BookKeeper/script.js
@@ -0,0 +1,22 @@
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkFrom = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
+
+function showModal() {
+  modal.classList.add("show-modal");
+  websiteNameEl.focus();
+}
+
+function closeModal() {
+  modal.classList.remove("show-modal");
+}
+
+modalShow.addEventListener("click", showModal);
+modalClose.addEventListener("click", closeModal);
+window.addEventListener("click", (e) =>
+  e.target === modal ? closeModal() : false
+);
