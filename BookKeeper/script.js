06eb2abc2b706f8cdfca3d6a79d49673bf819e00
InKyoJeong <viapolar@naver.com>
2021-06-20 01:45:05 +0900

    feat(bookmark-app): paint bookmarks

--- BookKeeper/script.js
+++ BookKeeper/script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById("website-name");
 const websiteUrlEl = document.getElementById("website-url");
 const bookmarksContainer = document.getElementById("bookmarks-container");
 
+let bookmarks = [];
+
 function showModal() {
   modal.classList.add("show-modal");
   websiteNameEl.focus();
@@ -39,6 +41,55 @@ function validate(nameValue, urlValue) {
   return true;
 }
 
+// Pain Bookmarks DOM
+function paintBookmarks() {
+  bookmarks.forEach((bookmark) => {
+    const { name, url } = bookmark;
+    console.log(name, url);
+
+    const item = document.createElement("div");
+    item.classList.add("item");
+
+    const closeIcon = document.createElement("i");
+    closeIcon.classList.add("fas", "fa-times");
+    closeIcon.id = "delete-bookmark";
+    closeIcon.setAttribute("title", "Delete Bookmark");
+    closeIcon.setAttribute("onclick", `deleteBookmark('${url}')`);
+
+    const linkInfo = document.createElement("div");
+    linkInfo.classList.add("name");
+
+    const favicon = document.createElement("img");
+    favicon.setAttribute(
+      "src",
+      `https://s2.googleusercontent.com/s2/favicons?domain=${url}`
+    );
+    favicon.setAttribute("alt", "Favicon");
+
+    const link = document.createElement("a");
+    link.setAttribute("href", `${url}`);
+    link.setAttribute("target", "_blank");
+    link.textContent = name;
+
+    //Append to bookmarks Container
+    linkInfo.append(favicon, link);
+    item.append(closeIcon, linkInfo);
+    bookmarksContainer.appendChild(item);
+  });
+}
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+  if (localStorage.getItem("bookmarks")) {
+    bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  } else {
+    bookmarks = [{ name: "INGG", url: "https://ingg.dev" }];
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  }
+
+  paintBookmarks();
+}
+
 function storeBookmark(e) {
   e.preventDefault();
   const nameValue = websiteNameEl.value;
@@ -47,10 +98,22 @@ function storeBookmark(e) {
     urlValue = `https://${urlValue}`;
   }
   if (!validate(nameValue, urlValue)) {
-    return false;
+    return false; // false면 밑으로는 skip
   }
-  console.log(nameValue, urlValue);
+
+  const bookmark = {
+    name: nameValue,
+    url: urlValue,
+  };
+  bookmarks.push(bookmark);
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+  fetchBookmarks();
+  bookmarkForm.reset();
+  websiteNameEl.focus();
 }
 
 // Event listener
 bookmarkForm.addEventListener("submit", storeBookmark);
+
+// On load, fetch bookmarks
+fetchBookmarks();
