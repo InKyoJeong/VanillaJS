8d0a04ac77b72af23db028d6c67c5f140dee878a
InKyoJeong <viapolar@naver.com>
2021-08-15 18:21:37 +0900

    feat(bankist-web) : tab content, hover header

--- BankistWeb/script.js
+++ BankistWeb/script.js
@@ -8,6 +8,12 @@ const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const nav = document.querySelector(".nav");
+
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -65,3 +71,55 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     behavior: "smooth",
   });
 });
+
+// Tab
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  // Avtive content area
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// Menu fade animation
+// mouseenter : does not bubble
+// const handleHover = function (e, opacity) {
+//   if (e.target.classList.contains("nav__link")) {
+//     const link = e.target;
+//     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+//     siblings.forEach((el) => {
+//       if (el !== link) el.style.opacity = opacity;
+//     });
+//   }
+// };
+
+// nav.addEventListener("mouseover", function (e) {
+//   handleHover(e, 0.5);
+// });
+
+// nav.addEventListener("mouseout", function (e) {
+//   handleHover(e, 1);
+// });
+
+const handleHover = function (e) {
+  // bind() 메서드에 전달된 첫 번째 인수는 바인딩된 함수 내부의 'this' 키워드에 할당
+  // console.log(this);
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+nav.addEventListener("mouseout", handleHover.bind(1));
+// bind returns new function
