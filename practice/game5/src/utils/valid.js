37e848a06064159aa4f2a30a855b752479fac204
InKyoJeong <viapolar@naver.com>
2021-11-09 19:47:59 +0900

    refactor(game5): Make check price and unit function

--- practice/game5/src/utils/valid.js
+++ practice/game5/src/utils/valid.js
@@ -1,6 +1,17 @@
 import { MESSAGE, NUM } from '../constants/index.js';
 import { displayAlert } from './displayAlert.js';
 
+const checkPriceAndUnit = value => {
+  let isValid = true;
+
+  if (value < NUM.MIN_PRICE || value % NUM.MIN_PRICE_UNIT !== 0) {
+    displayAlert(MESSAGE.PRICE_OR_UNIT_IS_LOW);
+    isValid = false;
+  }
+
+  return isValid;
+};
+
 export const isValidProductInput = (name, price, quantity) => {
   let isValid = true;
 
@@ -12,8 +23,7 @@ export const isValidProductInput = (name, price, quantity) => {
     displayAlert(MESSAGE.QUANTITY_IS_LOW);
     isValid = false;
   }
-  if (price < NUM.MIN_PRICE || price % NUM.MIN_PRICE_UNIT !== 0) {
-    displayAlert(MESSAGE.PRICE_OR_UNIT_IS_LOW);
+  if (!checkPriceAndUnit(price)) {
     isValid = false;
   }
 
@@ -23,8 +33,7 @@ export const isValidProductInput = (name, price, quantity) => {
 export const isValidChargeInput = charge => {
   let isValid = true;
 
-  if (charge < NUM.MIN_PRICE || charge % NUM.MIN_PRICE_UNIT !== 0) {
-    displayAlert(MESSAGE.PRICE_OR_UNIT_IS_LOW);
+  if (!checkPriceAndUnit(charge)) {
     isValid = false;
   }
 
@@ -34,8 +43,7 @@ export const isValidChargeInput = charge => {
 export const isValidPurchaseInput = amount => {
   let isValid = true;
 
-  if (amount < NUM.MIN_PRICE || amount % NUM.MIN_PRICE_UNIT !== 0) {
-    displayAlert(MESSAGE.PRICE_OR_UNIT_IS_LOW);
+  if (!checkPriceAndUnit(amount)) {
     isValid = false;
   }
 
