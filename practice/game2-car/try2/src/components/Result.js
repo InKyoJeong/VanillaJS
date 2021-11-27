f65a646f4693ebd2d1d2d59a010a1bffa729f89b
InKyoJeong <viapolar@naver.com>
2021-11-27 23:40:51 +0900

    feat(car): print advance result
    - 횟수에따른 전진 결과를 출력

--- practice/game2-car/try2/src/components/Result.js
+++ practice/game2-car/try2/src/components/Result.js
@@ -2,7 +2,8 @@ import Car from "../classes/Car.js";
 
 class Result {
   constructor({ $resultContainer, state }) {
-    $resultContainer.removeAttribute("hidden");
+    this.$resultContainer = $resultContainer;
+    this.$resultContainer.removeAttribute("hidden");
     this.state = state;
     this.cars = [];
     this.makeCar(this.state.names, this.state.count);
@@ -16,8 +17,23 @@ class Result {
   advanceCar(count) {
     while (count) {
       this.cars.forEach((car) => car.advance());
+      this.printResult();
       count--;
     }
+  }
+
+  printResult() {
+    this.cars.forEach((car) => {
+      // console.log(car);
+      const div = document.createElement("div");
+      const span = document.createElement("span");
+      span.innerText = `${car.name}: ${car.distance}`;
+      div.append(span);
+      this.$resultContainer.append(div);
+    });
+
+    const br = document.createElement("br");
+    this.$resultContainer.append(br);
   }
 }
 
