78331d30a58e07d9147f3468116c3d2d50b24501
InKyoJeong <viapolar@naver.com>
2021-11-28 16:24:47 +0900

    refactor(car): make game class

---
+++ practice/game2-car/try2/src/classes/Game.js
@@ -0,0 +1,23 @@
+import Car from "./Car.js";
+
+class Game {
+  constructor(cars) {
+    this.cars = cars.map((name) => new Car(name));
+  }
+
+  advanceCar() {
+    this.cars.forEach((car) => car.advance());
+    return this.cars;
+  }
+
+  getWinner() {
+    let maxDistance = 0;
+    this.cars.forEach((car) => {
+      maxDistance = Math.max(maxDistance, car.distance.length);
+    });
+
+    return this.cars.filter((car) => car.distance.length === maxDistance);
+  }
+}
+
+export default Game;
--- practice/game2-car/try2/src/components/Result.js
+++ practice/game2-car/try2/src/components/Result.js
@@ -1,52 +1,36 @@
 import Car from "../classes/Car.js";
+import Game from "../classes/Game.js";
 
 class Result {
   constructor({ $resultContainer, state }) {
     this.$resultContainer = $resultContainer;
     this.$resultContainer.removeAttribute("hidden");
     this.state = state;
-    this.cars = [];
-    this.makeCar(this.state.names, this.state.count);
+
+    this.game = new Game(this.state.names);
+    this.printResult(this.state.count);
   }
 
-  makeCar(names, count) {
-    names.forEach((name) => this.cars.push(new Car(name)));
-    this.advanceCar(count);
-  }
-
-  advanceCar(count) {
+  printResult(count) {
     while (count) {
-      this.cars.forEach((car) => car.advance());
-      this.printResult();
+      const results = this.game.advanceCar();
+      results.forEach(({ name, distance }) => {
+        const div = document.createElement("div");
+        const span = document.createElement("span");
+        span.innerText = `${name}: ${distance}`;
+        div.append(span);
+        this.$resultContainer.append(div);
+      });
+      const br = document.createElement("br");
+      this.$resultContainer.append(br);
       count--;
     }
 
     this.printWinner();
   }
 
-  printResult() {
-    this.cars.forEach((car) => {
-      const div = document.createElement("div");
-      const span = document.createElement("span");
-      span.innerText = `${car.name}: ${car.distance}`;
-      div.append(span);
-      this.$resultContainer.append(div);
-    });
-    const br = document.createElement("br");
-    this.$resultContainer.append(br);
-  }
-
-  getWinner() {
-    let maxDistance = 0;
-    this.cars.forEach((car) => {
-      maxDistance = Math.max(maxDistance, car.distance.length);
-    });
-
-    return this.cars.filter((car) => car.distance.length === maxDistance);
-  }
-
   printWinner() {
-    let winner = this.getWinner();
+    let winner = this.game.getWinner();
     winner = winner.reduce((acc, cur) => {
       return acc.concat(cur.name);
     }, []);
