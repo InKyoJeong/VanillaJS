23245848bcfbde0521a056acdfad0c032667556a
InKyoJeong <viapolar@naver.com>
2021-12-01 16:07:08 +0900

    feat(car3): get car name value
    - submit 이벤트리스너를 추가하여 자동차 이름을 입력받을 수 있도록 추가

--- practice/game2-car/try3/src/constants/index.js
+++ practice/game2-car/try3/src/constants/index.js
@@ -3,5 +3,6 @@ export const ID = {
   CAR_NAMES_SUBMIT: "car-names-submit",
   RACING_COUNT_INPUT: "racing-count-input",
   RACING_COUNT_SUBMIT: "racing-count-submit",
+  RESULT_CONTAINER: "result-container",
   RACING_WINNERS: "racing-winners",
 };
