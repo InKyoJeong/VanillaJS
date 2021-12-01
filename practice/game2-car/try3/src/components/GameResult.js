aac7ff7fd7396598909c2a434fb1e4b3b6b65a73
InKyoJeong <viapolar@naver.com>
2021-12-01 17:27:58 +0900

    feat(car3): advance car
    - 시도횟수에 맞게 자동차를 전진시키기

--- practice/game2-car/try3/src/components/GameResult.js
+++ practice/game2-car/try3/src/components/GameResult.js
@@ -4,7 +4,17 @@ class GameResult {
   constructor({ $resultContainer, state }) {
     this.$resultContainer = $resultContainer;
     this.state = state;
+
     this.game = new Game(this.state.names);
+    this.printResults(this.state.count);
+  }
+
+  printResults(count) {
+    while (count) {
+      const advanceResults = this.game.advanceCar();
+      console.log(advanceResults);
+      count--;
+    }
   }
 }
 
