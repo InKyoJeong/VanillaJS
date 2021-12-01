3c4a380a08cc298be1da8e78b9007b6ad5cb2600
InKyoJeong <viapolar@naver.com>
2021-12-01 16:31:16 +0900

    feat(car3): check name is valid
    - 각 자동차 이름이 5자를 초과했는지 검사하기
    - 자동차 이름이 5자를 초과했는지 검사하기
    - 빈값을 입력했는지 체크하기
    - 중복된 이름이 있는지 체크하기

--- practice/game2-car/try3/src/constants/index.js
+++ practice/game2-car/try3/src/constants/index.js
@@ -6,3 +6,13 @@ export const ID = {
   RESULT_CONTAINER: "result-container",
   RACING_WINNERS: "racing-winners",
 };
+
+export const NUM = {
+  MAX_NAME_LENGTH: 5,
+};
+
+export const ERROR = {
+  NAME_LENGTH_IS_LONG: `${NUM.MAX_NAME_LENGTH}자를 초과한 이름이 존재합니다.`,
+  NAME_IS_DUPLICATED: "중복된 이름이 존재합니다.",
+  NAME_IS_EMPTY: "빈값인 이름이 존재합니다.",
+};
---
+++ practice/game2-car/try3/src/utils/valid.js
@@ -0,0 +1,30 @@
+import { ERROR, NUM } from "../constants/index.js";
+
+const isExceedNameLength = (names) => {
+  return names.some((name) => name.length > NUM.MAX_NAME_LENGTH);
+};
+
+const isNameEmpty = (names) => {
+  return names.some((name) => name.trim().length === 0);
+};
+
+const isDuplicatedName = (names) => {
+  return new Set([...names]).size !== names.length;
+};
+
+export const isValidNames = (names) => {
+  if (isExceedNameLength(names)) {
+    alert(ERROR.NAME_LENGTH_IS_LONG);
+    return false;
+  }
+  if (isNameEmpty(names)) {
+    alert(ERROR.NAME_IS_EMPTY);
+    return false;
+  }
+  if (isDuplicatedName(names)) {
+    alert(ERROR.NAME_IS_DUPLICATED);
+    return false;
+  }
+
+  return true;
+};
