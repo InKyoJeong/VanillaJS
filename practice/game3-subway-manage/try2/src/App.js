a18ebfbdb113a7a10d307eebe95fcffc501ca76e
InKyoJeong <viapolar@naver.com>
2021-12-07 17:09:02 +0900

    feat(subway-map): add menu buttons

---
+++ practice/game3-subway-manage/try2/src/App.js
@@ -0,0 +1,39 @@
+import { ID } from "./constants/index.js";
+
+class App {
+  constructor($target) {
+    this.$target = $target;
+    this.addTemplate();
+    this.selectDom();
+    this.addEvent();
+  }
+
+  addTemplate() {
+    this.$target.innerHTML += `  
+        <div id=${ID.MANAGER_BUTTON_CONTAINER}>
+          <button id=${ID.STATION_MANAGER_BUTTON}>1. 역 관리</button>
+          <button id=${ID.LINE_MANAGER_BUTTON}>2. 노선 관리</button>
+          <button id=${ID.SECTION_MANAGER_BUTTON}>3. 구간 관리</button>
+          <button id=${ID.MAP_PRINT_MANAGER_BUTTON}>4. 지하철 노선도 출력</button>
+        </div>
+        <div id=${ID.RESULT_CONTAINER}></div>
+    `;
+  }
+
+  selectDom() {
+    this.$buttonContainer = document.querySelector(
+      `#${ID.MANAGER_BUTTON_CONTAINER}`
+    );
+    this.$resultContainer = document.querySelector(`#${ID.RESULT_CONTAINER}`);
+  }
+
+  addEvent() {
+    this.$buttonContainer.addEventListener("click", this.selectMenu.bind(this));
+  }
+
+  selectMenu(e) {
+    console.log(e.target.id);
+  }
+}
+
+export default App;
---
+++ practice/game3-subway-manage/try2/src/constants/index.js
@@ -0,0 +1,8 @@
+export const ID = {
+  MANAGER_BUTTON_CONTAINER: "manager-button-container",
+  RESULT_CONTAINER: "result-container",
+  STATION_MANAGER_BUTTON: "station-manager-button",
+  LINE_MANAGER_BUTTON: "line-manager-button",
+  SECTION_MANAGER_BUTTON: "section-manager-button",
+  MAP_PRINT_MANAGER_BUTTON: "map-print-manager-button",
+};
