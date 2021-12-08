7ba32aa27d979cce65e4610b70f3056b36eb792c
InKyoJeong <viapolar@naver.com>
2021-12-08 18:51:50 +0900

    feat(machine): add alert to product valid

--- practice/game5-vending-machine/try2/src/utils/valid.js
+++ practice/game5-vending-machine/try2/src/utils/valid.js
@@ -1,22 +1,27 @@
+import { ERROR, NUM } from "../../constants/index.js";
+
 const isProductNameEmpty = (name) => {
   return name.trim() === "";
 };
 
 const isValidPrice = (price) => {
-  return price >= 100 && price % 10 === 0;
+  return price >= NUM.PRICE_MIN_COUNT && price % NUM.PRICE_MIN_UNIT === 0;
 };
 const isValidQuantity = (quantity) => {
-  return quantity >= 1 && Number.isInteger(quantity);
+  return quantity >= NUM.QUANTITY_MIN_COUNT && Number.isInteger(quantity);
 };
 
 export const isValidProductInput = (name, price, quantity) => {
   if (isProductNameEmpty(name)) {
+    alert(ERROR.PRODUCT_NAME_IS_BLANK);
     return false;
   }
   if (!isValidPrice(price)) {
+    alert(ERROR.PRICE_IS_NOT_CORRECT);
     return false;
   }
   if (!isValidQuantity(quantity)) {
+    alert(ERROR.QUANTITY_IS_NOT_CORRECT);
     return false;
   }
 
