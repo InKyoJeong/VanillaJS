ef79489aa17d10dd62974de812cedf8732702ef6
InKyoJeong <viapolar@naver.com>
2021-12-09 17:39:59 +0900

    feat(machine): check purchase available

--- practice/game5-vending-machine/try2/src/component/purchase/PurchaseTable.js
+++ practice/game5-vending-machine/try2/src/component/purchase/PurchaseTable.js
@@ -1,5 +1,7 @@
 import { purchaseTableTemplate } from "../../utils/template/purchaseTemplate.js";
 import { addTableStyle } from "../../utils/tableStyle.js";
+import { CLASS } from "../../../constants/index.js";
+import { isPurchaseAvailable } from "../../utils/valid.js";
 
 class PurchaseTable {
   constructor($target) {
@@ -10,11 +12,32 @@ class PurchaseTable {
 
   render() {
     this.addTemplate();
+    this.addEvent();
     addTableStyle();
   }
 
   addTemplate() {
     this.$target.innerHTML = purchaseTableTemplate();
+  }
+
+  addEvent() {
+    this.$target.addEventListener("click", this.clickButton.bind(this));
+  }
+
+  clickButton(e) {
+    if (!e.target.classList.contains(CLASS.PURCHASE_BUTTON)) {
+      return;
+    }
+
+    const tr = e.target.closest("tr");
+    const { productName } = tr.children[0].dataset;
+    const { productPrice } = tr.children[1].dataset;
+    const { productQuantity } = tr.children[2].dataset;
+    console.log(tr, productName, productPrice, productQuantity);
+
+    if (!isPurchaseAvailable(productPrice)) {
+      return;
+    }
   }
 }
 
