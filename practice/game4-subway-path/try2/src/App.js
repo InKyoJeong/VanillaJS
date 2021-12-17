b964a064ce386a35d7c804990df1b285a52ee273
InKyoJeong <viapolar@naver.com>
2021-12-17 19:10:12 +0900

    feat(path): set data

--- practice/game4-subway-path/try2/src/App.js
+++ practice/game4-subway-path/try2/src/App.js
@@ -2,6 +2,14 @@ class App {
   constructor($target) {
     this.$target = $target;
   }
+
+  addTemplate() {
+    this.$target.innerHTML = `
+      <h1>지하철 길찾기</h1>
+      <div></div>
+      <div></div>
+    `;
+  }
 }
 
 export default App;
---
+++ practice/game4-subway-path/try2/src/data/index.js
@@ -0,0 +1,61 @@
+export const station = [
+  {
+    name: "교대",
+  },
+  {
+    name: "강남",
+  },
+  {
+    name: "역삼",
+  },
+  {
+    name: "남부터미널",
+  },
+  {
+    name: "양재",
+  },
+  {
+    name: "양재시민의숲",
+  },
+  {
+    name: "매봉",
+  },
+];
+
+export const sections = [
+  {
+    name: ["교대", "강남"],
+    distance: 2,
+    time: 3,
+  },
+  {
+    name: ["강남", "역삼"],
+    distance: 3,
+    time: 2,
+  },
+  {
+    name: ["교대", "남부터미널"],
+    distance: 3,
+    time: 2,
+  },
+  {
+    name: ["남부터미널", "양재"],
+    distance: 6,
+    time: 5,
+  },
+  {
+    name: ["양재", "매봉"],
+    distance: 1,
+    time: 1,
+  },
+  {
+    name: ["강남", "양재"],
+    distance: 2,
+    time: 8,
+  },
+  {
+    name: ["양재", "양재시민의숲"],
+    distance: 10,
+    time: 3,
+  },
+];
