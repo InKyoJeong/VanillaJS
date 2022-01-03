e12b70c03173e7f61ac2f022efd382d0bcb02535
InKyoJeong <viapolar@naver.com>
2022-01-03 18:24:42 +0900

    feat(store): add component

--- practice/simple-store/component/src/App.js
+++ practice/simple-store/component/src/App.js
@@ -1,5 +1,36 @@
-class App {
-  //
+import Component from "./core/Component.js";
+
+class App extends Component {
+  initState() {
+    return {
+      a: 1,
+      b: 2,
+    };
+  }
+
+  template() {
+    const { a, b } = this.state;
+
+    return `
+        <input id="stateA" value="${a}" />  
+        <input id="stateB" value="${b}" />  
+        <p>a+b = ${a + b}</p>
+    `;
+  }
+
+  setEvent() {
+    this.$target
+      .querySelector("#stateA")
+      .addEventListener("change", ({ target }) => {
+        this.state.a = Number(target.value);
+      });
+
+    this.$target
+      .querySelector("#stateB")
+      .addEventListener("change", ({ target }) => {
+        this.state.b = Number(target.value);
+      });
+  }
 }
 
 export default App;
---
+++ practice/simple-store/component/src/core/Component.js
@@ -0,0 +1,36 @@
+import { observable, observe } from "./observer.js";
+
+class Component {
+  constructor($target, props) {
+    this.$target = $target;
+    this.props = props;
+    this.setUp();
+  }
+
+  setUp() {
+    this.state = observable(this.initState());
+    observe(() => {
+      this.render();
+      this.setEvent();
+      this.mounted();
+    });
+  }
+
+  initState() {
+    return {};
+  }
+
+  template() {
+    return "";
+  }
+
+  render() {
+    this.$target.innerHTML = this.template();
+  }
+
+  setEvent() {}
+
+  mounted() {}
+}
+
+export default Component;
