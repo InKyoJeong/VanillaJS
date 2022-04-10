735722dbfe2593dcf526152f4edb0a48b158e0ba
InKyoJeong <viapolar@naver.com>
2022-04-11 04:47:21 +0900

    feat(vending-flux): add reducer, action

---
+++ practice/vending-flux/src/core/reducer.js
@@ -0,0 +1,10 @@
+import { initState } from "./initState.js";
+
+const reducer = (state = initState, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
